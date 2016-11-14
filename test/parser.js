861ccbe911bf417a5b264bd83e794add19a7edee
Patrick Boyd <pboyd04@gmail.com>
2016-11-14 08:38:37 -0600

    Add TypeDefinition and Function parsing

--- test/parser.js
+++ test/parser.js
@@ -3,5 +3,7 @@ var csdl = require('../index');
 module.exports.parse = function(assert) {
   var metadata = csdl.parseMetadataFile(__dirname + '/fixtures/SimpleMetadata.xml');
   console.log(metadata);
+  assert.notEqual(Object.keys(metadata.Functions).length, 0);
+  console.log(metadata.Functions);
   assert.done();
 }
