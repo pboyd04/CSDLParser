b6f2cf057e3df99a71e0b6c0d3b741c821a576cb
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-11-14 15:06:42 -0600

    Add parsing for all types in OData Example service

--- test/parser.js
+++ test/parser.js
@@ -4,6 +4,5 @@ module.exports.parse = function(assert) {
   var metadata = csdl.parseMetadataFile(__dirname + '/fixtures/SimpleMetadata.xml');
   console.log(metadata);
   assert.notEqual(Object.keys(metadata.Functions).length, 0);
-  console.log(metadata.Functions);
   assert.done();
 }
