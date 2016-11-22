d566ce216eb439de61d2e1f12fdad2265f377c9b
Patrick Boyd <pboyd04@gmail.com>
2016-11-21 22:34:19 -0600

    Add tests for CSDLsearch

---
+++ test/search.js
@@ -0,0 +1,21 @@
+var csdl = require('../index');
+
+module.exports.search = function(assert) {
+  var metadata = csdl.parseMetadataFile(__dirname + '/fixtures/SimpleMetadata.xml', {}, function(error, metadata) {
+    if(error) {
+      assert.ok(false, 'Failed to parse!');
+      console.log(error);
+      return;
+    }
+    var person = csdl.findByType(metadata, 'Microsoft.OData.Service.Sample.TrippinInMemory.Models.Person');
+    assert.equal(person.Name, 'Person');
+
+    var favoriteAirline = csdl.search(metadata, 'Function', 'GetFavoriteAirline');
+    assert.notEqual(favoriteAirline[0], undefined);
+    assert.equal(favoriteAirline[0].Name, 'GetFavoriteAirline');
+    assert.equal(favoriteAirline[0].IsBound, true);
+
+    assert.done();
+  });
+}
+/* vim: set tabstop=2 shiftwidth=2 expandtab: */
