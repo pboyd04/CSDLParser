1677ae8e5498a80609b4830e60732760303e3ec4
Patrick Boyd <Patrick_Boyd@Dell.com>
2019-02-27 11:34:30 -0600

    Switch to Non-Native XML Parser

--- test/uriParser.js
+++ test/uriParser.js
@@ -3,13 +3,14 @@ var assert = require('assert');
 
 describe('Uri Parsing', function() {
   it('Bad URI', function(done) {
-    csdl.parseMetadataUri('http://docs.oasis-open.org/odata/odata/v4.0/errata03/csd01/complete/vocabularies/Org.OData.Core.V1.xml', {}, function(error, metadata) {
+    csdl.parseMetadataUri('http://example.com/404', {}, function(error, metadata) {
       assert.notEqual(error, null);
       assert.equal(metadata, null);
       done();
     });
   });
   it('OData Core Parsing', function(done) {
+    this.timeout(10000);
     csdl.parseMetadataUri('http://docs.oasis-open.org/odata/odata/v4.0/errata03/csd01/complete/vocabularies/Org.OData.Core.V1.xml', {}, function(error, metadata) {
       assert.equal(error, null);
       assert.equal(Object.keys(metadata).length, 3);
